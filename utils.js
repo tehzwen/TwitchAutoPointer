cb9ddd51af7f50ddfe0521f3a5da3546f877d581
tehzwen <zachthm@gmail.com>
2021-08-08 00:05:35 -0600

    Fix Document Query * Change query for points button to use aria-label of "Claim Bonus" as   Twitch seems to have updated the elements to use random/dynamically   generated ids

--- utils.js
+++ utils.js
@@ -2,21 +2,21 @@
  * Gets the desired element on the client page and clicks on it
  */
 function clickPointsButton() {
-    var activityTab = document.getElementsByClassName("tw-button--success tw-interactive")[0];
-    if (activityTab) {
-        activityTab.click();
-        chrome.runtime.sendMessage({ message: "Clicked successfully" });
-    }
+  var activityTab = document.querySelector('[aria-label="Claim Bonus"]');
+  if (activityTab) {
+    activityTab.click();
+    chrome.runtime.sendMessage({ message: "Clicked successfully" });
+  }
 }
 
 function runClicker() {
-    setTimeout(() => {
-        chrome.runtime.sendMessage({ message: "Trying to click..." });
-        clickPointsButton();
-        runClicker();
-    }, 3000);
+  setTimeout(() => {
+    chrome.runtime.sendMessage({ message: "Trying to click..." });
+    clickPointsButton();
+    runClicker();
+  }, 10000);
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-    runClicker();
+  runClicker();
 });
